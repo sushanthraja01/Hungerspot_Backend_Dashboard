31bf215757f9888c416244ecb24e599e71c85fe3
Raja Sai Datha Sushanth <rajasaidathasushanth2006@gmail.com>
2025-08-06 23:13:24 +0530

    second change

--- src/vendordashboard/helpers/ApiPath.js
+++ src/vendordashboard/helpers/ApiPath.js
@@ -1,2 +1,2 @@
-export const API_PATH = "http://localhost:2006/"
-// export const API_PATH = "https://backend-nodejs-hungerstop.onrender.com/"
+// export const API_PATH = "http://localhost:2006/"
+export const API_PATH = "https://backend-nodejs-hungerstop.onrender.com/"
